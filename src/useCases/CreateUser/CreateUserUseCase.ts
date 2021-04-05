858055f619c5995a33057088048971d0d0fda08a
Kojikun1 <bruno.chen.duarte@hotmail.com>
2021-04-05 11:41:19 -0300

    create user controller and email provider

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,10 +1,12 @@
 import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvider";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserRequestDTO } from './CreateUserDTO';
 
 export class CreateUserUserCase {
     constructor(
-        private usersRepository: IUsersRepository
+        private usersRepository: IUsersRepository,
+        private mailProvider: IMailProvider
     ){}
     async execute(data: ICreateUserRequestDTO ){
         const userAlreadyExists = await this.usersRepository.findByEmail(data.email);
@@ -16,5 +18,18 @@ export class CreateUserUserCase {
         const user = new User(data);
 
         await this.usersRepository.save(user);
+
+        await this.mailProvider.sendMail({
+            to: {
+                name: data.name,
+                email: data.email,
+            },
+            from : {
+                name: "Equipe do Meu App",
+                email: "equipe@meuapp.com"
+            },
+            subject: "Seja bem-vindo à plataforma",
+            body: "<p>Você já pode fazer login em nossa plataforma.</p>"
+        });
     }
 }
